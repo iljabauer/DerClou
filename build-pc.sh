77ea954c475b9f9899666a0045c65f25ba2f42a3
Ilja Bauer <iljabauer@gmail.com>
2024-10-09 17:07:11 +0200

    Added sh for building for the non web aka pc

---
+++ build-pc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf build
+conan install . --output-folder=build --build=missing
+cd build
+cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build . -v
