76b1dee683251acb19f835b6bcb4f87a68ea5411
Ilja Bauer <iljabauer@gmail.com>
2026-01-05 13:49:10 +0000

    Add scene constants and story-related constants
    Add all SCENE_* constants from story.h for story progression.
    Add picture IDs, story text file ID, and game costs.
    
    Co-authored-by: Ona <no-reply@ona.com>

--- src-js/src/game/types/GameConstants.ts
+++ src-js/src/game/types/GameConstants.ts
@@ -39,3 +39,140 @@ export const TXT_MENU_TIMEOUT = 254;
 // Talk modes
 export const DLG_TALKMODE_BUSINESS = 1;
 export const DLG_TALKMODE_STANDARD = 2;
+
+// Scene IDs (from story.h)
+export const SCENE_NEW_GAME = 45350912;
+export const SCENE_CREDITS = 46399488;
+
+export const SCENE_GARAGE = 16777216;
+export const SCENE_PARKING = 17039360;
+export const SCENE_CARS_OFFICE = 2621440;
+export const SCENE_TOOLS = 3145728;
+
+export const SCENE_PARKER = 19398656;
+export const SCENE_MALOYA = 17825792;
+export const SCENE_POOLY = 19136512;
+
+export const SCENE_TOWER_OUT = 15204352;
+export const SCENE_AT_SABIEN = 45088768;
+export const SCENE_FREIFAHRT = 21233664;
+export const SCENE_ARRIVAL = 22282240;
+export const SCENE_STATION = 21495808;
+export const SCENE_HOTEL_REC = 18612224;
+export const SCENE_HOTEL_1ST_TIME = 22544384;
+export const SCENE_DANNER = 22806528;
+export const SCENE_GLUDO_MONEY = 23068672;
+export const SCENE_HOLLAND_STR = 20185088;
+export const SCENE_HOTEL = 18612224;
+export const SCENE_LISSON_GROVE = 33292288;
+export const SCENE_CARS_VANS = 1048576;
+export const SCENE_HOTEL_ROOM = 2097152;
+export const SCENE_FAT_MANS = 2359296;
+export const SCENE_MAMI_CALLS = 23855104;
+export const SCENE_FST_MEET_BRIGGS = 24117248;
+export const SCENE_CALL_FROM_POOLY = 31457280;
+export const SCENE_WALRUS = 16515072;
+export const SCENE_WATLING = 20971520;
+export const SCENE_THE_END = 3932160;
+export const SCENE_FAHNDUNG = 24641536;
+export const SCENE_1ST_BURG = 24903680;
+export const SCENE_GLUDO_SAILOR = 25165824;
+export const SCENE_CALL_BRIGGS = 25427968;
+export const SCENE_2ND_BURG = 25690112;
+export const SCENE_3RD_BURG = 25952256;
+export const SCENE_4TH_BURG = 26214400;
+export const SCENE_5TH_BURG = 26476544;
+export const SCENE_6TH_BURG = 28573696;
+export const SCENE_7TH_BURG = 28835840;
+export const SCENE_8TH_BURG = 29097984;
+export const SCENE_9TH_BURG = 29360128;
+export const SCENE_POLICE = 3407872;
+export const SCENE_ARRESTED_MATT = 26738688;
+export const SCENE_POOLY_AFRAID = 27787264;
+export const SCENE_MALOYA_AFRAID = 28049408;
+export const SCENE_PARKER_AFRAID = 28311552;
+export const SCENE_RAID = 30932992;
+export const SCENE_DART = 31195136;
+export const SCENE_MEETING_AGAIN = 38273024;
+
+export const SCENE_GLUDO_BURNS = 27000832;
+export const SCENE_MORNING = 31981568;
+export const SCENE_VISITING = 32243712;
+export const SCENE_A_DREAM = 35127296;
+export const SCENE_ROSENBLATT = 35389440;
+export const SCENE_BRIGGS_ANGRY = 35651584;
+export const SCENE_SABIEN_WALRUS = 35913728;
+export const SCENE_SABIEN_DINNER = 36175872;
+export const SCENE_TOMBOLA = 36438016;
+export const SCENE_PRESENT_HOTEL = 36700160;
+export const SCENE_INFO_TOWER = 36962304;
+export const SCENE_RAINY_EVENING = 37224448;
+
+export const SCENE_SABIEN_CALL = 38010880;
+export const SCENE_BIRTHDAY = 38797312;
+export const SCENE_WALK_WITH = 39059456;
+export const SCENE_AGENT = 39321600;
+export const SCENE_JAGUAR = 44302336;
+export const SCENE_THINK_OF = 40370176;
+export const SCENE_TERROR = 40108032;
+export const SCENE_CONFESSING = 40632320;
+
+export const SCENE_PRISON = 44826624;
+
+export const SCENE_SOUTH_WITHOUT = 44040192;
+export const SCENE_SOUTH_UNKNOWN = 41418752;
+export const SCENE_SOUTHHAMPTON = 43778048;
+
+export const SCENE_MISSED_DATE_0 = 37748736;
+export const SCENE_MISSED_DATE_1 = 43515904;
+
+export const SCENE_VILLA_INSIDE = 8650752;
+export const SCENE_KENWO_INSIDE = 9175040;
+
+export const SCENE_TANTE_INSIDE = 5242880;
+export const SCENE_JUWEL_INSIDE = 5767168;
+export const SCENE_ANTIQ_INSIDE = 6291456;
+export const SCENE_TRANS_INSIDE = 6815744;
+export const SCENE_SENIO_INSIDE = 7340032;
+export const SCENE_HAMHO_INSIDE = 9699328;
+export const SCENE_OSTER_INSIDE = 10223616;
+export const SCENE_CHISW_INSIDE = 10747904;
+export const SCENE_SOTHE_INSIDE = 11796480;
+export const SCENE_BRITI_INSIDE = 12320768;
+export const SCENE_NATUR_INSIDE = 12845056;
+export const SCENE_NATIO_INSIDE = 13369344;
+export const SCENE_VICTO_INSIDE = 13893632;
+export const SCENE_BANKO_INSIDE = 14417920;
+export const SCENE_TOWER_INSIDE = 14942208;
+
+export const SCENE_KASERNE_INSIDE = 47185920;
+export const SCENE_KASERNE_OUTSIDE = 47448064;
+
+export const SCENE_PROFI_21 = 52428800;
+export const SCENE_PROFI_22 = 51380224;
+export const SCENE_PROFI_23 = 48496640;
+export const SCENE_PROFI_24 = 51904512;
+export const SCENE_PROFI_25 = 47972352;
+export const SCENE_PROFI_26 = 50855936;
+export const SCENE_PROFI_27 = 50069504;
+export const SCENE_PROFI_28 = 50593792;
+
+export const SCENE_PROFI_21_INSIDE = 52166656;
+export const SCENE_PROFI_22_INSIDE = 51118080;
+export const SCENE_PROFI_23_INSIDE = 48234496;
+export const SCENE_PROFI_24_INSIDE = 51642368;
+export const SCENE_PROFI_25_INSIDE = 47710208;
+export const SCENE_PROFI_27_INSIDE = 49807360;
+export const SCENE_PROFI_28_INSIDE = 50331648;
+
+// Picture IDs
+export const PHONE_PICTID = 144;
+export const LETTER_PICTID = 151;
+export const OLD_MATT_PICTID = 0; // TODO: Find actual value
+export const MATT_PICTID = 0; // TODO: Find actual value
+
+// Story text file
+export const STORY_0_TXT = 18; // TODO: Verify this value
+
+// Game costs
+export const tcCOSTS_FOR_HOTEL = 100; // TODO: Find actual value
