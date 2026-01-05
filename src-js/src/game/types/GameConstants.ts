c6fdd45279449b5bb93347a844cc78a3cddc6a38
Ilja Bauer <iljabauer@gmail.com>
2026-01-05 15:01:05 +0000

    Add Person_Red_Stanson constant
    Co-authored-by: Ona <no-reply@ona.com>

--- src-js/src/game/types/GameConstants.ts
+++ src-js/src/game/types/GameConstants.ts
@@ -14,6 +14,7 @@ export const Person_Dan_Stanford = 9821;
 export const Person_John_Gludo = 9822;
 export const Person_Eric_Pooly = 9825;
 export const Person_Sabien_Pardo = 9846;
+export const Person_Red_Stanson = 9847;
 export const Person_Pater_James = 9848;
 
 // Key Location IDs
