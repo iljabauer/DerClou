509756ae2859a173f10e815d4ae2fd435509b05f
Ilja Bauer <iljabauer@gmail.com>
2026-01-06 19:19:15 +0000

    Set updateSnapshots to none in playwright config
    Co-authored-by: Ona <no-reply@ona.com>

--- src-js/playwright.config.ts
+++ src-js/playwright.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
+    /* Disable snapshot updates */
+    updateSnapshots: 'none',
 
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
